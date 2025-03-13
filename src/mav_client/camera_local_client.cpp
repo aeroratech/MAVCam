d079133e5566d6fd820e76e37e276a174b7e731a
thomas.an <xueshen.an@aeroratech.com>
2025-03-13 12:25:56 +0800

    support iso 320

--- src/mav_client/camera_local_client.cpp
+++ src/mav_client/camera_local_client.cpp
@@ -272,7 +272,7 @@ mavsdk::CameraServer::Result CameraLocalClient::fill_information(
         information.vertical_resolution_px = in_info.vertical_resolution_px;
         information.lens_id = in_info.lens_id;
         //TODO (Thomas) : hard code
-        information.definition_file_version = 13;
+        information.definition_file_version = 14;
         information.definition_file_uri = "mftp://definition/D64TR.xml";
     } else {
         information.vendor_name = "Unknown";
